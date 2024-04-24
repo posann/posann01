e8c43b93100080270b488391ea8d2983976edda7
Ahmad Fauzan <57349734+posann@users.noreply.github.com>
2024-04-25 00:35:57 +0800

    Update workhistory.js

--- app/utils/constant/workhistory.js
+++ app/utils/constant/workhistory.js
@@ -10,26 +10,6 @@ const WORK_HISTORY = [
         duration: 'JAN 2022-APR 2022',
     },
     {
-        company: 'Pertamina Gas Negara',
-        position: 'Frontend Developer',
-        duration: 'NOV 2023-FEB 2024',
-    },
-    {
-        company: 'Resikrek',
-        position: 'Mobile Developer',
-        duration: 'DEC 2023-FEB 2024',
-    },
-    {
-        company: 'Resikrek',
-        position: 'Full Stack Developer',
-        duration: 'OCT 2023-DEC 2023',
-    },
-    {
-        company: 'YBM PLN',
-        position: 'Mobile Developer',
-        duration: 'MAR 2024-JUN 2024',
-    },
-    {
         company: 'PeTIK',
         position: 'Programming Lecturer',
         duration: 'MAY 2022-CURRENT',
